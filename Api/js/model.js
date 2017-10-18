246ec99651f71a1bbfa85e32605231922a7fa6f2
Bruno Brancalhao <brunobrancalhao26@gmail.com>
2017-10-17 23:31:09 -0200

    Subindo o projeto

---
+++ Api/js/model.js
@@ -0,0 +1,52 @@
+var express = require('express')
+var app = express()
+var bodyParser = require('body-parser')
+var core_use = require('cors');
+var sql = require("mssql");
+
+app.use(core_use());
+app.use(bodyParser.urlencoded({ extended: false }))
+app.use(bodyParser.json())
+
+var config = {
+    server: '172.21.29.26',
+    database: 'hackatonlaranja',
+    user: 'cwo',
+    password: 'zo42jl76',
+    idleTimeoutMillis: 30000
+};
+
+var retorno = {
+    method: '',
+    uri: '',
+    response: []
+};
+
+app.get('/consultarCartaoSus', function (req, res) {
+    //var request = new testwe.Request();
+    sql.connect(config, function (err) {
+        var request = new sql.Request();
+        console.log(req.body);
+        console.log('select top 1 p.nome as NOME_PACIENTE, P.Cartao_SUS as CARTAO_SUS_PACIENTE from Pacientes P where (1=1) AND P.Cartao_SUS = \'' + req.body.cartaoSus + '\'');
+        // query to the database and get the records ''
+        request.query('select top 1 p.nome as NOME_PACIENTE, P.Cartao_SUS as CARTAO_SUS_PACIENTE from Pacientes P where (1=1) AND P.Cartao_SUS = \'' + req.body.cartaoSus + '\'', function (err, response) {
+            if (err) {
+                sql.close();
+                console.log(err);
+            };
+            retorno.method = 'GET';
+            retorno.uri = 'consultarCartaoSus';
+            retorno.response = response.recordsets;
+
+            res.setHeader('Access-Control-Allow-Origin', '*');
+            res.json(retorno);
+            sql.close();
+        });
+
+    });
+
+});
+
+var server = app.listen(5000, function () {
+    console.log('Server is running..');
+});
